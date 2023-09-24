50355f1b2568083302303f0e790098f82c1d3346
liujiaodaliao <mengmeijia@126.com>
2023-09-24 22:18:53 +0200

    declarer tableau contenant plusieurs diapositives（image tagline）
     Ajoutez des Event Listeners
    declarer un tableau contenant chemin d'image et une phrase associée *4

--- assets/script.js
+++ assets/script.js
@@ -1,18 +1,24 @@
-const slides = [
+document.addEventListener("DOMContentLoaded", function () {
+    //1 添加事件监听器
+    const slides = [
 	{
-		"image":"slide1.jpg",
+		"image":"./assets/images/slideshow/slide1.jpg",
 		"tagLine":"Impressions tous formats <span>en boutique et en ligne</span>"
 	},
 	{
-		"image":"slide2.jpg",
+		"image":"./assets/images/slideshow/slide2.jpg",
 		"tagLine":"Tirages haute définition grand format <span>pour vos bureaux et events</span>"
 	},
 	{
-		"image":"slide3.jpg",
+		"image":"./assets/images/slideshow/slide3.jpg",
 		"tagLine":"Grand choix de couleurs <span>de CMJN aux pantones</span>"
 	},
 	{
-		"image":"slide4.png",
+		"image":"./assets/images/slideshow/slide4.png",
 		"tagLine":"Autocollants <span>avec découpe laser sur mesure</span>"
 	}
 ]
+//2 创建常量slides tableaux，定义image和标语tagline
+
+});
+//这段代码主要实现了一个基本的轮播功能，当页面加载完毕后会自动轮播幻灯片，用户也可以通过点击箭头或圆点手动切换幻灯片。
