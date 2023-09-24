3dabbccd091b6a9348189aba87e4c21595d7f944
liujiaodaliao <mengmeijia@126.com>
2023-09-24 22:33:19 +0200

    Ajout de la fonctionnalité de mise à jour de l'image et dot dans la fonction autoSlide
    Cette modification ajoute la fonctionnalité de mise à jour de l'image de la diapositive dans la fonction carrousel. Elle garantit que l'image correspondant à la diapositive en cours est affichée correctement.

--- assets/script.js
+++ assets/script.js
@@ -31,6 +31,34 @@ let currentIndex = 0;
     const nextArrow = banner.querySelector(".arrow_right");
     const tagLine = banner.querySelector("p");
 
+    //7 多次初始化tagline更新标语，故创建标语函数，简化代码
+    function updateTagLine(){
+        tagLine.innerHTML = slides[currentIndex].tagLine;
+        image.src = slides[currentIndex].image; 
+    }
+    //7 停止自动轮播函数，清除之前设置的自动轮播定时器
+    function stopAutoSlide() {
+        clearInterval(autoSlideInterval); 
+    }
+
+    // 5 创建自动轮播autoSlide函数，
+    //当前页圆点取消选中状态
+    //为轮播控制索引，实现切换下一页
+    //赋值后是下一页，圆点选中
+    //调用函数更新标语和图片
+    function autoSlide() {
+        dots[currentIndex].classList.remove("dot_selected");
+        currentIndex = (currentIndex + 1) % slides.length;
+        dots[currentIndex].classList.add("dot_selected");
+        updateTagLine();
+    }
+
+    //6 设置定时器函数，触发时，每3秒调用一次自动轮播函数
+    let autoSlideInterval = setInterval(autoSlide, 3000);
+
+    //7 初始化第一张幻灯片
+    updateTagLine(); 
+
    
 });
 //这段代码主要实现了一个基本的轮播功能，当页面加载完毕后会自动轮播幻灯片，用户也可以通过点击箭头或圆点手动切换幻灯片。
