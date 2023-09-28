8bd85e64713fc7b57a3aa10eeb6543d24e113bb9
liujiaodaliao <mengmeijia@126.com>
2023-09-28 22:53:20 +0200

    ajout commentaire en francais

--- assets/script.js
+++ assets/script.js
@@ -1,5 +1,5 @@
 document.addEventListener("DOMContentLoaded", function () {
-    //1 添加事件监听器
+    //1 添加事件监听器 ecouteur d'evenement
     const slides = [
 	{
 		"image":"./assets/images/slideshow/slide1.jpg",
@@ -21,27 +21,29 @@ document.addEventListener("DOMContentLoaded", function () {
 //2 创建常量slides tableaux，定义image和标语tagline   
 
 let currentIndex = 0; 
-//3 当前幻灯片索引index,以便切换幻灯片，初始值为0 表示初始状态下显示第一张幻灯片
+//3 当前幻灯片索引variable index,以便切换幻灯片，初始值为0 表示初始状态下显示第一张幻灯片
 
-    //4 获取轮播相关的 DOM 元素,获取容器,4个轮播图，圆点数组，左箭头，右箭头，标语p
+    //4 recuperer les elements 获取轮播相关的 DOM 元素,获取容器, 4个轮播图，圆点数组，左箭头，右箭头，标语p
     const banner = document.getElementById("banner");
     const image = banner.querySelector("img"); 
+    const tagLine = banner.querySelector("p");
     const dots = banner.querySelectorAll(".dot");
     const prevArrow = banner.querySelector(".arrow_left");
     const nextArrow = banner.querySelector(".arrow_right");
-    const tagLine = banner.querySelector("p");
 
-    //7 多次初始化tagline更新标语，故创建标语函数，简化代码
-    function updateTagLine(){
+
+    //7 fonction tagline simplifier le code (mise à jour images et textes)更新标语，故创建标语函数，在调用函数，简化代码,
+    function updateTagLineImg(){
         tagLine.innerHTML = slides[currentIndex].tagLine;
         image.src = slides[currentIndex].image; 
     }
-    //7 停止自动轮播函数，清除之前设置的自动轮播定时器
+    //7 arrêter le carrousel automatique.
+    //停止自动轮播函数，清除之前设置的自动轮播定时器
     function stopAutoSlide() {
         clearInterval(autoSlideInterval); 
     }
 
-    // 5 创建自动轮播autoSlide函数，
+    // 5 fonction autoSlide
     //当前页圆点取消选中状态
     //为轮播控制索引，实现切换下一页
     //赋值后是下一页，圆点选中
@@ -50,23 +52,23 @@ let currentIndex = 0;
         dots[currentIndex].classList.remove("dot_selected");
         currentIndex = (currentIndex + 1) % slides.length;
         dots[currentIndex].classList.add("dot_selected");
-        updateTagLine();
+        updateTagLineImg();
     }
 
-    //6 设置定时器函数，触发时，每3秒调用一次自动轮播函数
+    //6 minuterie设置定时器函数，触发时，每3秒调用一次自动轮播函数
     let autoSlideInterval = setInterval(autoSlide, 3000);
 
     //7 初始化第一张幻灯片
-    updateTagLine(); 
+    updateTagLineImg(); 
 
-    //8 左箭头点击事件，
+    //8 fleche evenement左箭头点击事件，
     //点击事件监听器 点击触发时，先停止轮播，点一次切换一页，圆点同步，3s后再次启动轮播
     prevArrow.addEventListener("click", function () {
         stopAutoSlide();
         dots[currentIndex].classList.remove("dot_selected");
         currentIndex = (currentIndex - 1 + slides.length) % slides.length;
         dots[currentIndex].classList.add("dot_selected");
-        updateTagLine(); 
+        updateTagLineImg(); 
         autoSlideInterval = setInterval(autoSlide, 3000); 
         console.log("The left arrow was clicked"); 
         // 测试事件监听器是否成功运行
@@ -78,12 +80,12 @@ let currentIndex = 0;
         dots[currentIndex].classList.remove("dot_selected");
         currentIndex = (currentIndex + 1) % slides.length;
         dots[currentIndex].classList.add("dot_selected");
-        updateTagLine(); 
+        updateTagLineImg(); 
         autoSlideInterval = setInterval(autoSlide, 3000); 
         console.log("The right arrow was clicked"); 
     });
 
-    //10 点击圆点切换幻灯片
+    //10 dot evenement 点击圆点切换幻灯片
     dots.forEach(function (dot, index) {
         //用forEach方法每个圆点执行回调函数（参数1当前元素dot和当前元素的在数组中的索引），点击时切换，自动轮播停止，不点击时再重新启动轮播
         dot.addEventListener("click", function () {  
@@ -91,7 +93,7 @@ let currentIndex = 0;
             dots[currentIndex].classList.remove("dot_selected");
             currentIndex = index;
             dots[currentIndex].classList.add("dot_selected");
-            updateTagLine(); 
+            updateTagLineImg(); 
             autoSlideInterval = setInterval(autoSlide, 3000); 
             console.log("Dots have been updated");
         });
